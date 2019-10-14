826835373ee587a1c063f2836ce2896153761cf2
Tom Augspurger <thomas-augspurger@uiowa.edu>
2019-10-14 15:00:58 -0500

    fixups

--- config.sh
+++ config.sh
@@ -24,10 +24,10 @@ function build_wheel {
 
 function pip_opts {
     # Add pre-release index until official NumPy release with 3.8
-    if [-n "$MANYLINUX_URL" ]; then
-        echo "--find-links $MANYLINUX_URL --find-links='https://7933911d6844c6c53a7d-47bd50c35cd79bd838daf386af554a83.ssl.cf2.rackcdn.com'"
+    if [ -n "$MANYLINUX_URL" ]; then
+        echo "--find-links $MANYLINUX_URL --find-links=https://7933911d6844c6c53a7d-47bd50c35cd79bd838daf386af554a83.ssl.cf2.rackcdn.com"
     else
-        echo "--find-links='https://7933911d6844c6c53a7d-47bd50c35cd79bd838daf386af554a83.ssl.cf2.rackcdn.com'"
+        echo "--find-links=https://7933911d6844c6c53a7d-47bd50c35cd79bd838daf386af554a83.ssl.cf2.rackcdn.com"
     fi
 }
 
