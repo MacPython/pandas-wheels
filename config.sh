4cbd977ae635c0720fff8c7407eec83d93cd8d75
Tom Augspurger <TomAugspurger@users.noreply.github.com>
2019-10-15 08:33:04 -0500

    Daily 3.8 (#63)
    * Bump multibuild
    
    * Add 3.8 build
    
    * pre-install setuptools_scm only for mac 3.5 build
    
    (cherry picked from commit 1508d26c84da930bbf571b5e51521343ff948b1c)

--- config.sh
+++ config.sh
@@ -21,6 +21,17 @@ function build_wheel {
     build_bdist_wheel $@
 }
 
+
+function pip_opts {
+    # Add pre-release index until official NumPy release with 3.8
+    if [ -n "$MANYLINUX_URL" ]; then
+        echo "--find-links $MANYLINUX_URL --find-links=https://7933911d6844c6c53a7d-47bd50c35cd79bd838daf386af554a83.ssl.cf2.rackcdn.com"
+    else
+        echo "--find-links=https://7933911d6844c6c53a7d-47bd50c35cd79bd838daf386af554a83.ssl.cf2.rackcdn.com"
+    fi
+}
+
+
 function run_tests {
     # Runs tests on installed distribution from an empty directory
     export PYTHONHASHSEED=$(python -c 'import random; print(random.randint(1, 4294967295))')
