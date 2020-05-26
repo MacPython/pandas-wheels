82776058a01d552ca6624c469e17b9b389372cc3
Tom Augspurger <tom.w.augspurger@gmail.com>
2020-05-26 16:15:07 -0500

    added env_vars

---
+++ env_vars.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# Environment variables for build
+MACOSX_DEPLOYMENT_TARGET=10.9
+# CFLAGS override for https://github.com/pandas-dev/pandas/issues/34114
+CFLAGS="-Wno-error=maybe-uninitialized -Wno-error=sign-compare"
+# Macos's linker doesn't support stripping symbols
+if [ "$(uname)" != "Darwin" ]; then
+    LDFLAGS="-Wl,--strip-debug"
+    # make sure that LDFLAGS is exposed to child processes,
+    # since current version of manybuild only export CFLAGS, CPPFLAGS and FFLAGS
+    export LDFLAGS="$LDFLAGS"
+    # override the default stripping flags, since we only override CFLAGS and
+    # the current version of manybuild pass "-strip-all" to CPPFLAGS and FFLAGS
+    STRIP_FLAGS=""
+fi
+# For verbosity: report where each command came from
+export PS4='+(${BASH_SOURCE}:${LINENO}): ${FUNCNAME[0]:+${FUNCNAME[0]}(): }
+set -x
