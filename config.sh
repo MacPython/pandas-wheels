56dcd1c176dbd65efef722ed53b624fad12bc5dc
Simon Hawkins <simonjayhawkins@gmail.com>
2020-11-26 17:48:47 +0000

    enable tests for py37

--- config.sh
+++ config.sh
@@ -33,5 +33,5 @@ function run_tests {
     # Skip test_maybe_promote_int_with_int: https://github.com/pandas-dev/pandas/issues/31856
     # Skip test_float_precision_options: https://github.com/pandas-dev/pandas/issues/36429
     # TestPandasContainer for 3.7.0 failure
-    python -c 'import sys; import pandas; pandas.test(extra_args=["-m not clipboard", "--skip-slow", "--skip-network", "--skip-db", "-n=2", "-k not test_maybe_promote_int_with_int and not test_file_descriptor_leak and not test_float_precision_options"]) if sys.version_info[:2] != (3, 7) else None'
+    python -c 'import sys; import pandas; pandas.test(extra_args=["-m not clipboard", "--skip-slow", "--skip-network", "--skip-db", "-n=2", "-k not test_maybe_promote_int_with_int and not test_file_descriptor_leak and not test_float_precision_options"])'
 }
