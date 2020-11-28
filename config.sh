992d95142df2a54fdf145cf78e5854af64af53e3
Simon Hawkins <simonjayhawkins@gmail.com>
2020-11-28 12:02:04 +0000

    add skips for test_rolling_var_numerical_issues and test_rolling_skew_kurt_large_value_range

--- config.sh
+++ config.sh
@@ -31,5 +31,7 @@ function run_tests {
     pip list
     python -c 'import pandas; pandas.show_versions()'
     # Skip test_float_precision_options: https://github.com/pandas-dev/pandas/issues/36429
-    python -c 'import pandas; pandas.test(extra_args=["-m not clipboard", "--skip-slow", "--skip-network", "--skip-db", "-n=2", "-k not test_float_precision_options"])'
+    # Skip test_rolling_var_numerical_issues: https://github.com/pandas-dev/pandas/issues/37398
+    # Skip test_rolling_skew_kurt_large_value_range: https://github.com/pandas-dev/pandas/issues/37398
+    python -c 'import pandas; pandas.test(extra_args=["-m not clipboard", "--skip-slow", "--skip-network", "--skip-db", "-n=2", "-k not test_rolling_var_numerical_issues and not test_rolling_skew_kurt_large_value_range and not test_float_precision_options"])'
 }
