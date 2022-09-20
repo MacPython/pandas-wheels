5fccac8abb6ab352e1b6e6af76edc32048605852
Thomas Li <47963215+lithomas1@users.noreply.github.com>
2022-09-20 10:58:46 -0400

    single cpu has replaced high memory marker

--- config.sh
+++ config.sh
@@ -42,5 +42,5 @@ function run_tests {
     # Skip test_rolling_var_numerical_issues: https://github.com/pandas-dev/pandas/issues/37398
     # Skip test_rolling_skew_kurt_large_value_range: https://github.com/pandas-dev/pandas/issues/37398
     # Skip test_pairwise_with_self/test_no_pairwise_with_self: https://github.com/pandas-dev/pandas/issues/39553
-    python -c 'import pandas; pandas.test(extra_args=["-m not clipboard", "--skip-slow", "--skip-network", "--skip-db", "-n=2", "-k not test_rolling_var_numerical_issues and not test_rolling_skew_kurt_large_value_range and not test_float_precision_options and not test_pairwise_with_self and not test_no_pairwise_with_self and not test_pickle_frame_v124_unpickle_130"])'
+    python -c 'import pandas; pandas.test(extra_args=["-m not clipboard", "--skip-slow", "--skip-network", "--skip-db", "-n=2", "-k not test_rolling_var_numerical_issues and not test_rolling_skew_kurt_large_value_range and not test_float_precision_options and not test_pairwise_with_self and not test_no_pairwise_with_self and not test_pickle_frame_v124_unpickle_130 and not single_cpu"])'
 }
